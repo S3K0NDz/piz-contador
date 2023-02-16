33a21c69eae7392252e0c6c4a9824d7e9ab02644
S3K0NDz <49118731+S3K0NDz@users.noreply.github.com>
2023-02-16 12:00:18 +0100

    Update piz.ino

--- piz.ino
+++ piz.ino
@@ -1,17 +1,49 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0X27, 16, 2); 
+
+
+
 
 int contador = 0;
-void setup(){
-  Serial.begin(9600);
-  pinMode(7, INPUT);
-  
+void setup() {
+  Serial.begin(300);
+  lcd.init();             //Inicia lcd    
+  lcd.backlight();        //Retroiluminación lcd     
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+
 }
-void loop(){
-  if(digitalRead(3)== HIGH){
-    if(digitalRead(3)== LOW){
-      contador++;
-      delay(5);
-      Serial.print(contador);
+
+void loop() {
+
+  if (digitalRead(3) == HIGH) {
+    if (digitalRead(3) == LOW) {
+    lcd.clear();
+    contador++; 
+    lcd.setCursor(4,0);
+    lcd.print("Pizarras");
+    lcd.setCursor(7,1);
+    lcd.print(contador);
     }
-    }
+  }
+
+  if (digitalRead(4) == HIGH) {
+    if (digitalRead(4) == LOW) {
+    lcd.clear();
+    contador = 0;
+    lcd.setCursor(4,0);
+    lcd.print("Pizarras");
+    lcd.setCursor(7,1);
+    lcd.print(contador);
  
+    }
+  }
+
 }
+
+
+
+
+
+
